af993b7c699b621e603a70061fe18abcab71b504
bagussadewa12 <117002631+bagussadewa12@users.noreply.github.com>
2025-08-29 16:31:04 +0800

    fixed model

--- app/Models/post.php
+++ app/Models/post.php
@@ -4,13 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class post extends Model
+class Post extends Model   // <-- huruf besar P
 {
-  protected $fillable = [
+    protected $fillable = [
         'title',
         'slug',
         'content',
         'author',
-        'thumbnail', // ganti dari image ke thumbnail
+        'thumbnail',
     ];
 }
